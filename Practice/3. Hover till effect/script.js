263f099687a019881b32838055819a694e5fcb8c
Piyush Gupta <Guptapiyush4444@gmail.com>
2023-10-16 13:57:14 +0530

    update

---
+++ Practice/3. Hover till effect/script.js
@@ -0,0 +1,11 @@
+let mode = document.querySelector("#mode");
+mode.onclick = function () {
+    if (mode.classList.contains("ri-moon-fill")) {
+        mode.classList.replace("ri-moon-fill", "ri-sun-fill");
+        document.body.classList.replace("lighttheme", "darktheme");
+    }
+    else {
+        mode.classList.replace("ri-sun-fill", "ri-moon-fill");
+        document.body.classList.replace("darktheme", "lighttheme");
+    }
+};
