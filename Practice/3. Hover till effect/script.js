869c1217e87b248543c11a16bab6f15582ecfa7a
Piyush Gupta <Guptapiyush4444@gmail.com>
2023-10-19 18:59:31 +0530

    update

--- Practice/3. Hover till effect/script.js
+++ Practice/3. Hover till effect/script.js
@@ -9,3 +9,36 @@ mode.onclick = function () {
         document.body.classList.replace("darktheme", "lighttheme");
     }
 };
+
+
+
+let crsr = document.querySelector("#cursor");
+let blur1 = document.querySelector("#cursor-blur");
+
+
+
+document.addEventListener("mousemove", function (dets) {
+    crsr.style.left = dets.x - 8 + "px";
+    crsr.style.top = dets.y - 8 + "px";
+    // blur1.style.left = dets.x - 200 + "px";
+    // blur1.style.top = dets.y - 200 + "px";
+});
+
+
+let all = document.querySelectorAll(".logo, .nav-containt, .cards");
+let h1 = document.querySelector("#web");
+let color = h1.style.color;
+all.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        crsr.style.transition = "scale 0.3s ease";
+        crsr.style.scale = 4;
+        crsr.style.border = "1px solid #d1d1d1";
+        crsr.style.backgroundColor = "transparent";
+    });
+
+    elem.addEventListener("mouseleave", function () {
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #d1d1d1";
+        crsr.style.backgroundColor = "#d1d1d1";
+    });
+});
